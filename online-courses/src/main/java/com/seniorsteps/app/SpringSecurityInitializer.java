fd01f087bdfe28130ead9eac62ce24dfef605be8
MostafaElfeky <mostafa.ahmed.elfeky@gmail.com>
2020-03-01 08:27:10 +0200

    updates

--- online-courses/src/main/java/com/seniorsteps/app/SpringSecurityInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.seniorsteps.app;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-// initialize spring security filter chain
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
