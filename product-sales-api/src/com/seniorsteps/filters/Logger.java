4944d2bd1c263bbdb67ed1ca85acfdfac950add8
MostafaElfeky <mostafa.ahmed.elfeky@gmail.com>
2020-05-11 00:44:11 +0200

    updates

--- product-sales-api/src/com/seniorsteps/filters/Logger.java
+++ product-sales-api/src/com/seniorsteps/filters/Logger.java
@@ -6,6 +6,7 @@ import java.util.Map.Entry;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -37,6 +38,18 @@ public class Logger implements Filter {
 		
 		// pass the request along the filter chain
 		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
